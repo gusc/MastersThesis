0e38fb564933f9ce92eea3ddddca990a4cb251e9
Gusts Kaksis <gusts.kaksis@gmail.com>
2019-05-19 02:47:08 -0700

    Use heap allocations instead of stack for buffers. Refactoring.

--- Source/MCAPI_DFT_Core1.c
+++ Source/MCAPI_DFT_Core1.c
@@ -24,7 +24,7 @@ char __argv_string[] = "";
 #define CPU_PORT_NUM	3 // Core1 connects to 101, Core2 to 102
 
 #define MAX_DATA_SIZE (MAX_MESSAGE_SIZE - sizeof(message_header_t))
-#define MAX_SAMPLE_COUNT ((MAX_MESSAGE_SIZE - sizeof(message_header_t)) / sizeof(complex_float_t))
+#define MAX_SAMPLE_COUNT 29 //(MAX_DATA_SIZE / sizeof(complex_float_t))
 
 /*
  * mcapiErrorCheck()
@@ -74,116 +74,86 @@ int main(int argc, char *argv[])
 
 	printf("Starting core 1 processor\n");
 
-	bool end_process_received = false;
 	size_t recv_size = 0;
 	struct
 	{
 		message_header_t header;
 		complex_float_t data[MAX_SAMPLE_COUNT];
 	} remote_data;
+
 	struct
 	{
 		message_header_t header;
-		char data[256];
-	} error_data;
-	error_data.header.type = MSG_ERROR;
+		complex_float_t data[MAX_SAMPLE_COUNT];
+	} out_data;
 
-	while (!end_process_received)
+	complex_float_t* buffer = (complex_float_t*)calloc(MAX_BUFFER_SIZE, sizeof(complex_float_t));
+	complex_float_t* out_buffer = (complex_float_t*)calloc(MAX_BUFFER_SIZE, sizeof(complex_float_t));
+	size_t offset = 0;
+	size_t N = MAX_BUFFER_SIZE;
+	bool end_process = false;
+	while (!end_process)
 	{
-		mcapi_uint_t res = 0;
-		while (res == 0)
-		{
-			res = mcapi_msg_available(local_ep, &mcapi_status);
-			mcapiErrorCheck(mcapi_status, "msg_available", 2);
-		}
-
 		mcapi_msg_recv(local_ep, &remote_data, sizeof(remote_data), &recv_size, &mcapi_status);
 		mcapiErrorCheck(mcapi_status, "msg_recv", 2);
 
-		if (mcapi_status == MCAPI_SUCCESS)
-		{
-			if (remote_data.header.type == MSG_END_PROCESS)
-			{
-				// End process
-				end_process_received = true;
+		size_t data_len = remote_data.header.length * sizeof(complex_float_t);
+		data_len = min(data_len, (MAX_BUFFER_SIZE - offset) * sizeof(complex_float_t));
+		memcpy(buffer + offset, remote_data.data, data_len);
+		offset += remote_data.header.length;
 
-				mcapi_msg_send(local_ep, remote_ep, &remote_data, sizeof(message_header_t), 0, &mcapi_status);
+		if (remote_data.header.message_index == remote_data.header.total_count - 1)
+		{
+			N = remote_data.header.total_length;
+			message_t processing_type = remote_data.header.type;
+			if (processing_type == MSG_DFT_BUFFER)
+			{
+				dft(buffer, out_buffer, N, 0);
+			}
+			else if (processing_type == MSG_IDFT_BUFFER)
+			{
+				dft(buffer, out_buffer, N, 1);
+			}
+			else if (processing_type == MSG_FFT_BUFFER)
+			{
+				Radix2FFT(buffer, out_buffer, N);
+			}
+			else if (processing_type == MSG_IFFT_BUFFER)
+			{
+				Radix2iFFT(buffer, out_buffer, N);
+			}
+
+			// Send split buffers
+			size_t num_messages = (N + MAX_SAMPLE_COUNT - 1) / MAX_SAMPLE_COUNT;
+			for (size_t i = 0; i < num_messages; i ++)
+			{
+				size_t out_offset = i * MAX_SAMPLE_COUNT;
+				size_t num_samples = N - out_offset;
+				if (num_samples > MAX_SAMPLE_COUNT)
+				{
+					num_samples = MAX_SAMPLE_COUNT;
+				}
+
+				out_data.header.type = processing_type;
+				out_data.header.length = num_samples;
+				out_data.header.total_length = N;
+				out_data.header.total_count = num_messages;
+				out_data.header.message_index = i;
+
+				memcpy(out_data.data, out_buffer + out_offset, out_data.header.length * sizeof(complex_float_t));
+
+				size_t out_data_len = sizeof(message_header_t) + (out_data.header.length * sizeof(complex_float_t));
+
+				mcapi_msg_send(local_ep, remote_ep, &out_data, out_data_len, 0, &mcapi_status);
 				mcapiErrorCheck(mcapi_status, "msg_send", 2);
 			}
-			else
-			{
-				// Concatenate incoming buffers
-				size_t N = remote_data.header.total_length;
-				complex_float_t buffer[N];
-				complex_float_t out_buffer[N];
-				message_t processing_type = remote_data.header.type;
-				size_t data_len = remote_data.header.length * sizeof(complex_float_t);
-				memcpy(buffer, remote_data.data, data_len);
-				size_t offset = remote_data.header.length;
-				while (remote_data.header.message_index != remote_data.header.total_count - 1)
-				{
-					mcapi_msg_recv(local_ep, &remote_data, sizeof(remote_data), &recv_size, &mcapi_status);
-					mcapiErrorCheck(mcapi_status, "msg_recv", 2);
 
-					if (mcapi_status != MCAPI_SUCCESS)
-					{
-						break;
-					}
-
-					data_len = remote_data.header.length * sizeof(complex_float_t);
-					memcpy(buffer + offset, remote_data.data, data_len);
-					offset += remote_data.header.length;
-				}
-
-				// Do requested processing
-
-				if (processing_type == MSG_DFT_BUFFER)
-				{
-					dft(buffer, out_buffer, N, 0);
-				}
-				else if (processing_type == MSG_IDFT_BUFFER)
-				{
-					dft(buffer, out_buffer, N, 1);
-				}
-				else if (processing_type == MSG_FFT_BUFFER)
-				{
-					Radix2FFT(buffer, out_buffer, N);
-				}
-				else if (processing_type == MSG_IFFT_BUFFER)
-				{
-					Radix2iFFT(buffer, out_buffer, N);
-				}
-
-				// Send split buffers
-
-				size_t num_messages = (N + MAX_SAMPLE_COUNT - 1) / MAX_SAMPLE_COUNT;
-				for (size_t i = 0; i < num_messages; i ++)
-				{
-					size_t offset = i * MAX_SAMPLE_COUNT;
-					size_t num_samples = N - offset;
-					if (num_samples > MAX_SAMPLE_COUNT)
-					{
-						num_samples = MAX_SAMPLE_COUNT;
-					}
-
-					struct {
-						message_header_t header;
-						complex_float_t data[MAX_SAMPLE_COUNT];
-					} remote_data;
-					remote_data.header.type = processing_type;
-					remote_data.header.length = num_samples;
-					remote_data.header.total_length = N;
-					remote_data.header.total_count = num_messages;
-					remote_data.header.message_index = i;
-
-					memcpy(remote_data.data, out_buffer + offset, remote_data.header.length * sizeof(complex_float_t));
-
-					mcapi_msg_send(local_ep, remote_ep, &remote_data, sizeof(remote_data), 0, &mcapi_status);
-					mcapiErrorCheck(mcapi_status, "msg_send", 2);
-				}
-			}
+			offset = 0;
 		}
 	}
+
+	free(out_buffer);
+	free(buffer);
 
 	return 0;
 }
